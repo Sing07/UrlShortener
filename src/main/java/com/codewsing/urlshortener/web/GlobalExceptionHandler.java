409f916461fadbb87d2bc16948922538b827c3b7
SingT <61646543+Sing07@users.noreply.github.com>
2025-05-01 16:20:14 -0700

    added 404 and 500 error pages and handling of redirect

--- src/main/java/com/codewsing/urlshortener/web/GlobalExceptionHandler.java
+++ src/main/java/com/codewsing/urlshortener/web/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.codewsing.urlshortener.web;
 
 import com.codewsing.urlshortener.domain.exceptions.ShortUrlNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(ShortUrlNotFoundException.class)
     String handleShortUrlNotFoundException (ShortUrlNotFoundException ex) {
+        log.error("Short URL not found: {}", ex.getMessage());
         return "error/404";
+    }
 
+    @ExceptionHandler(Exception.class)
+    String handleException (ShortUrlNotFoundException ex) {
+        log.error("Unhandled Exception: {}", ex.getMessage(), ex);
+        return "error/500";
     }
 }
